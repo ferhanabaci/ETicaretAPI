48f467c53f85f402bf4691ce6961f8971f68f965
Ferhan ABACI <ferhan.abaci@santsg.com>
2023-02-28 21:41:42 +0300

    Generic Repository Design Pattern development

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Product/ProductReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ProductReadRepository : ReadRepository<Product>, IProductReadRepository
+    {
+        public ProductReadRepository(ETicaretAPIDbContext context) :  base(context)
+        {
+            
+        }
+    }
+}
